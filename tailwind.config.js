da3555b8444a1f17e9fbc569a8eda60634725df8
JaeHyunLee123 <jhyon123@gmail.com>
2023-06-25 00:33:43 +0900

    #3 Setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    //configuration of where tailwindcss is used
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
