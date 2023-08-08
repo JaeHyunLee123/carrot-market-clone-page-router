cb1542fad5097872f6753fd03692d459ed65c9e4
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-08-08 16:11:21 +0900

    #13.1 Reviews

---
+++ pages/api/reviews.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@libs/server/prismaClients";
+import withHandler, { IResposeType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<IResposeType>
+) => {
+  const {
+    session: { user },
+  } = req;
+
+  if (!user) return res.status(400).json({ ok: false });
+
+  const reviews = await prisma.review.findMany({
+    where: {
+      reviewedForId: user.id,
+    },
+    include: {
+      reviewedBy: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+
+  res.status(200).json({ ok: true, reviews });
+};
+
+export default withApiSession(
+  withHandler({ methods: ["GET"], handler, isPrivate: true })
+);
